1504e8633790fbb58d1a657e50c31da1472347b9
viniciuscfn <c.viniciusmagalhaes@gmail.com>
2021-06-12 21:37:15 -0300

    Criando Modulo Calculadora e adicionando o index.ts no modulo calculadora

--- Calculadora-Angular/src/app/app.component.spec.ts
+++ Calculadora-Angular/src/app/app.component.spec.ts
@@ -15,17 +15,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'Calculadora-Angular'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('Calculadora-Angular');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('Calculadora-Angular app is running!');
-  });
 });
